ad51038ec734ccb9a56150723be0cc89aae63aad
Great-Nw <graetnwanguma@gmail.com>
2026-08-26 13:29:11 +0100

    "New things added"

--- pandas1.py
+++ pandas1.py
@@ -1,53 +1,69 @@
 import pandas as pd
 
-data= {
-     'Name': ['Charles','Nick'],
-     'Age': [24,89] 
- }
+# data= {
+#      'Name': ['Charles','Nick'],
+#      'Age': [24,89] 
+#  }
 
- df = pd.DataFrame(data)
+#  df = pd.DataFrame(data)
 
- print(df)
-#Quick preview of dataframe
-print(df.head(1))
-#Statistical Description of dataframe
-print(df.describe())
-#Getting extra info about dataframe
-print(df.info())
-#Getting data from a specific column 
-print(df[['Name']])
-print(df['Age'])
+#  print(df)
+# #Quick preview of dataframe
+# print(df.head(1))
+# #Statistical Description of dataframe
+# print(df.describe())
+# #Getting extra info about dataframe
+# print(df.info())
+# #Getting data from a specific column 
+# print(df[['Name']])
+# print(df['Age'])
 
-#shape, returns a tuple containing values for rows and columns
-print(df.shape)
-#columns returns a list containing every column name in the dataframe
-print(df.columns)
-#dtypes return tehe data types for data stored in each columns
-print(df.dtypes)
+# #shape, returns a tuple containing values for rows and columns
+# print(df.shape)
+# #columns returns a list containing every column name in the dataframe
+# print(df.columns)
+# #dtypes return tehe data types for data stored in each columns
+# print(df.dtypes)
 
-#  PRACTICE PROBLEM
-#We are given a data to work with. We are to:
-#Turn the given data into a dataframe.
-#1.Extract items under the 'price' column
-#2.Use .info() to get the info of the product data
-#3.Use .describe() to get statistical information
+# #  PRACTICE PROBLEM
+# #We are given a data to work with. We are to:
+# #Turn the given data into a dataframe.
+# #1.Extract items under the 'price' column
+# #2.Use .info() to get the info of the product data
+# #3.Use .describe() to get statistical information
 
-#Product Data
-product_data = {
-    'name': ['Apple', 'Banana', 'Orange', 'Mango'],
-    'price': [0.5, 0.3, 0.6, 0.8],
-    'stock': [100, 120, 80, 60]
+# #Product Data
+# product_data = {
+#     'name': ['Apple', 'Banana', 'Orange', 'Mango'],
+#     'price': [0.5, 0.3, 0.6, 0.8],
+#     'stock': [100, 120, 80, 60]
+# }
+
+# #Creating Dataframe of product data
+# data_frame = pd.DataFrame(product_data)
+# print(data_frame)
+
+# #Extracting price column
+# print(data_frame['price'])
+
+# #Displaying extra information about Dataframe
+# print(data_frame.info())
+
+# #Getting Statistical information
+# print(data_frame.describe())
+
+#DATAFRAME BASICS
+#A dataframe is a 2d dimensional array unlike series
+new_data = {
+ 'name': ['Ally','Bob','Charles'],
+ #first column
+ 'age': [11,12,17],
+ #second column
+ 'city': ['NY','SF','LA']
 }
+newDf = pd.DataFrame(new_data)
+print(newDf)
 
-#Creating Dataframe of product data
-data_frame = pd.DataFrame(product_data)
-print(data_frame)
-
-#Extracting price column
-print(data_frame['price'])
-
-#Displaying extra information about Dataframe
-print(data_frame.info())
-
-#Getting Statistical information
-print(data_frame.describe())
+new_data = [{'name':'Ally','age':11, 'city':'NY'},
+            {'name':'Bob','age':12, 'city':'SF'}]
+print(pd.DataFrame(new_data))
